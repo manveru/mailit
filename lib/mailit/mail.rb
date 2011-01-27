4fc3345850c581df6889741959c42daf818d0003
Michael Fellinger <m.fellinger@gmail.com>
2011-01-27 09:18:47 -0600

    Fix typos in docs

--- lib/mailit/mail.rb
+++ lib/mailit/mail.rb
@@ -25,9 +25,9 @@ module Mailit
   #   mail = Mailit::Mail.new
   #   mail.to = 'test@test.com'
   #   mail.from = 'sender@sender.com'
-  #   mail.subject 'Here are some files for you!'
+  #   mail.subject = 'Here are some files for you!'
   #   mail.text = 'This is what people with plain text mail readers will see'
-  #   mail.html = "A little something <b>special</b> for people with HTML readers'
+  #   mail.html = 'A little something <b>special</b> for people with HTML readers'
   #   mail.attach('/etc/fstab')
   #   mail.attach('/home/manveru/.vimrc')
   #
